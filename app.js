40e514e90fa5a80e51e2403706d8d152ce19b9fd
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2021-10-12 21:39:36 -0400

    fully working index and css

--- app.js
+++ app.js
@@ -135,10 +135,20 @@ promptUser()
   .then(portfolioData => {
       const pageHTML = generatePage(portfolioData);
       
-      fs.writeFile('./index.html', pageHTML, err => {
-        if (err) throw err;
+      fs.writeFile('./dist/index.html', pageHTML, err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Page created! Check out index.html in this directory to see the output!');
 
-        console.log('Portfolio complete! Check out index.html to see the output!');
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+         console.log('Style sheet copied successfully!');   
+        })
       });
 
   });
